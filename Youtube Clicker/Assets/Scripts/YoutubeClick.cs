201e826815d2680002c588aba60ab2b6f8abb3be
ShiroW <shirow.pvp@gmail.com>
2018-12-22 15:00:33 +0100

    Essai 2

---
+++ Youtube Clicker/Assets/Scripts/YoutubeClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YoutubeClick : MonoBehaviour
+{
+
+    static int clickPower = 1;
+
+
+    private void OnMouseDown()
+    {
+        CounterDisplayer.subs+=clickPower;
+    }
+}
