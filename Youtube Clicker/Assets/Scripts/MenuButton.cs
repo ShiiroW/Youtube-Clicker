100aa337fbc939c592c83a96d2545e8acb0bc145
ShiroW <shirow.pvp@gmail.com>
2018-12-24 14:33:34 +0100

    trc

---
+++ Youtube Clicker/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+
+    public string scene;
+
+    private void OnMouseUp()
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+}
